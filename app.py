4277ecba7d86402ce6b0318f0411e8fae4435808
Casey M <caseym@gmail.com>
2021-01-31 20:00:16 -0600

    add sentry

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+# error reporting with sentry
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_DSN'),
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
 CORS(app)

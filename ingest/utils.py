6d61ebc99d160f9f3fa27f4c9f66507196e15142
Casey M <caseym@gmail.com>
2021-02-09 13:04:43 -0600

    create open access import class

--- ingest/utils.py
+++ ingest/utils.py
@@ -1,6 +1,47 @@
+import csv
+import gzip
 import unicodedata
+import urllib.request
 
+from app import db
 from models.journal import Journal
+
+
+class CSVImporter:
+    def __init__(self, table_name, url):
+        self.table_name = table_name
+        self.staging_table = table_name + '_staging'
+        self.url = url
+
+    def import_data(self):
+        self.create_temp_table()
+        self.copy_csv_to_temp_table()
+
+    def get_file(self):
+        """
+        Opens remote file.
+        """
+        response = urllib.request.urlopen(self.url)
+        gzip_file = gzip.GzipFile(fileobj=response)
+        return gzip_file
+
+    def copy_csv_to_temp_table(self):
+        """
+        Very fast way to copy 1 million or more rows into a table.
+        """
+        copy_sql = "COPY {} FROM STDOUT WITH (FORMAT CSV, DELIMITER ',', HEADER, ENCODING 'ISO_8859_5')".format(self.staging_table)
+        conn = db.engine.raw_connection()
+        with conn.cursor() as cur:
+            cur.copy_expert(copy_sql, self.get_file())
+        conn.commit()
+
+    def create_temp_table(self):
+        db.session.execute('CREATE TABLE {} ( like {} including all)'.format(self.staging_table, self.table_name))
+        db.session.commit()
+
+    def drop_temp_table(self):
+        db.session.execute('DROP TABLE IF EXISTS {}'.format(self.staging_table))
+        db.session.commit()
 
 
 def find_journal(issn):

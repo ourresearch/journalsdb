5b6d62da3d05234294cf5e097985fe676a81a9d7
Casey M <caseym@gmail.com>
2021-02-14 07:25:30 -0600

    merge database migrations to resolve duplicate head issue

---
+++ migrations/versions/25138f02595e_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 25138f02595e
+Revises: 23a3b558760d, c3449ed8577e
+Create Date: 2021-02-14 07:24:31.578944
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "25138f02595e"
+down_revision = ("23a3b558760d", "c3449ed8577e")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

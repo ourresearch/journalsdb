0f6dac985289e9f34bd8f03da35d8d1f02cda4bc
Casey M <caseym@gmail.com>
2021-03-02 15:20:00 -0600

    update rectraction watch import to include published year

--- ingest/retraction_watch.py
+++ ingest/retraction_watch.py
@@ -22,6 +22,7 @@ def import_retraction_watch():
 
     for index, row in df.iterrows():
         if not db.session.query(RetractionWatch).get(row["Record ID"]):
+            crossref_doi_api = call_crossref_api(row["OriginalPaperDOI"])
             r = RetractionWatch(
                 record_id=row["Record ID"],
                 title=row["Title"],
@@ -30,23 +31,43 @@ def import_retraction_watch():
                 retraction_date=row["RetractionDate"],
                 retraction_doi=row["RetractionDOI"],
                 paper_doi=row["OriginalPaperDOI"],
-                issn=get_issn(row["OriginalPaperDOI"]),
+                published_year=get_published_year(crossref_doi_api),
+                issn=get_issn(crossref_doi_api),
             )
             print("adding doi {}".format(row["OriginalPaperDOI"]))
             db.session.add(r)
             db.session.commit()
 
 
-def get_issn(doi):
+def call_crossref_api(doi):
     """
-    Look up ISSN via crossref metadata api.
+    Calls the crossref works API for a given DOI.
     """
     url = "http://api.crossref.org/works/{}".format(doi)
     headers = {
         "User-Agent": "JournalsDB/1.1 (https://journalsdb.org; mailto:team@ourresearch.org)"
     }
     r = requests.get(url, headers=headers)
-    time.sleep(0.5)
+    time.sleep(0.2)
+    return r
+
+
+def get_published_year(r):
+    """
+    Look up ISSN via crossref metadata api.
+    """
+    if r.status_code == 200:
+        message = r.json()["message"]
+        return message["issued"]["date-parts"][0][0] if message.get("issued") else None
+    else:
+        print("year not found")
+        return None
+
+
+def get_issn(r):
+    """
+    Look up ISSN via crossref metadata api.
+    """
     if r.status_code == 200:
         message = r.json()["message"]
         return message["ISSN"][0] if message.get("ISSN") else None

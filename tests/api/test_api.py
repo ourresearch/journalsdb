fd42f2fbe779cd8f03a649e68cc6bfbda505403c
Casey M <caseym@gmail.com>
2021-03-20 07:39:57 -0500

    refactor api tests

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -8,18 +8,10 @@ class TestAPI:
     def test_find_issn(self, client, run_import_issns_with_api):
         run_import_issns_with_api("ISSN-to-ISSN-L-api.txt")
 
-        rv = client.get("/journal/2291-5222")
+        rv = client.get("/journals/2291-5222")
         json_data = rv.get_json()
         assert rv.status_code == 200
         assert json_data["issn_l"] == "2291-5222"
         assert json_data["issns"] == ["2291-5222"]
-        assert json_data["title"] == "JMIR mhealth and uhealth."
+        assert json_data["title"] == "JMIR mhealth and uhealth"
         assert json_data["publisher"] == "JMIR Publications Inc."
-
-        rv = client.get("/journal/0000-0043")
-        json_data = rv.get_json()
-        assert rv.status_code == 200
-        assert json_data["issn_l"] == "0000-0043"
-        assert json_data["issns"] == ["0000-0043"]
-        assert json_data["title"] == "Irregular serials & annuals."
-        assert json_data["publisher"] == ""

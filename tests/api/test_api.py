8d52d34739a7a3bfcaffdd6d07993d7c070276a2
Casey M <caseym@gmail.com>
2021-03-20 10:04:11 -0500

    add tests for new journals endpoint

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -7,7 +7,6 @@ class TestAPI:
 
     def test_find_issn(self, client, run_import_issns_with_api):
         run_import_issns_with_api("ISSN-to-ISSN-L-api.txt")
-
         rv = client.get("/journals/2291-5222")
         json_data = rv.get_json()
         assert rv.status_code == 200
@@ -15,3 +14,35 @@ class TestAPI:
         assert json_data["issns"] == ["2291-5222"]
         assert json_data["title"] == "JMIR mhealth and uhealth"
         assert json_data["publisher"] == "JMIR Publications Inc."
+
+    def test_journals(self, client, run_import_issns_with_api):
+        run_import_issns_with_api("ISSN-to-ISSN-L-api.txt")
+        rv = client.get("/journals")
+        json_data = rv.get_json()
+        assert rv.status_code == 200
+        sample = next(
+            (item for item in json_data["journals"] if item["issn_l"] == "1907-1760"),
+            None,
+        )
+        assert sample
+
+        attrs = "issn_l,title"
+        rv = client.get("/journals?attrs={}".format(attrs))
+        json_data = rv.get_json()
+        assert rv.status_code == 200
+
+        attrs = "issn_l,title,uuid,publisher_name,publisher_synonyms,issns"
+        rv = client.get("/journals?attrs={}".format(attrs))
+        json_data = rv.get_json()
+        assert rv.status_code == 200
+
+        sample = next(
+            (item for item in json_data["journals"] if item["issn_l"] == "1907-1760"),
+            None,
+        )
+        assert sample["publisher_name"] == "Universitas Andalas"
+        assert sample["synonyms"] == None
+        assert "2460-6626" in sample["issns"]
+        assert "1907-1760" in sample["issns"]
+        assert sample["title"] == "Jurnal peternakan Indonesia"
+        assert sample["uuid"]

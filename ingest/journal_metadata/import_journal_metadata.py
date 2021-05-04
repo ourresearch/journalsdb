aca305e0de959a7d50c71d24cc04123b568ce44f
Casey M <caseym@gmail.com>
2021-05-04 15:55:27 -0500

    import springer journal metadata

--- ingest/journal_metadata/import_journal_metadata.py
+++ ingest/journal_metadata/import_journal_metadata.py
@@ -40,3 +40,12 @@ def import_sage_md(file_name):
     j = JournalMetaDataImporter(df)
     j.cleanse_data()
     j.ingest_metadata()
+
+
+@app.cli.command("import_springer_md")
+@click.option("--file_name", default="ingest/journal_metadata/Springer.csv")
+def import_springer_md(file_name):
+    df = pd.read_csv(file_name)
+    j = JournalMetaDataImporter(df)
+    j.cleanse_data()
+    j.ingest_metadata()

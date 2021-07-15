7739176545285b1b8396276d0b4b9b6a1cfbefc7
Casey M <caseym@gmail.com>
2021-07-15 12:28:54 -0500

    refactor out of class

--- ingest/issn/issn_commands.py
+++ ingest/issn/issn_commands.py
@@ -1,14 +1,13 @@
 from app import app
-from ingest.issn.issn_import_issns import ImportISSNs
+from ingest.issn.issn_import_issns import import_issns
 
 
 @app.cli.command("import_issns")
-def import_issns():
+def import_issns_command():
     """
     Master ISSN list: https://www.issn.org/wp-content/uploads/2014/03/issnltables.zip
     Available via a text file within the zip archive with name ISSN-to-ISSN-L-initial.txt.
 
     Read the issn-l to issn mapping from issn.org and save it to a table for further processing.
     """
-    i = ImportISSNs()
-    i.import_issns()
+    import_issns()
